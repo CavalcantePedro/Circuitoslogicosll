603bda92d9280673262df61a28e720d3423d34db
cavalcantepedro <pedro.ricardo@academico.ufpb.br>
2023-08-09 16:27:25 -0300

    Update

--- lista1a/lista1a.sv
+++ lista1a/lista1a.sv
@@ -1,11 +1,10 @@
 module lista1a
 (output logic [29:0]z,
-input logic[6:0]x);
+input logic[6:0]x); 
 
-always @ * begin
+always @ * begin //O always é usado para definir um bloco de código que será executado sempre que houver uma mudança nos sinais de entrada do módulo.
 if (x > 4)
 z =( x*x*x)+(5*x);
 else z= 2*x+5;
-
-end	
+end	//end é usado para finalizar um bloco de código.
 endmodule
---
+++ lista1ex2/lista1ex2.sv
@@ -0,0 +1,6 @@
+module lista1ex2
+(input logic [7:0] entrada);
+
+
+
+endmodule
